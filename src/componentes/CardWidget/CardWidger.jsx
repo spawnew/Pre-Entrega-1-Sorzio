12dc5f829ba29ecac549b2c4f9f36583a1b4ac88
spawnew <96084099+spawnew@users.noreply.github.com>
2023-11-05 20:10:49 -0300

    creando componentes

---
+++ src/componentes/CardWidget/CardWidger.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CardWidger = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CardWidger
---
+++ src/componentes/ItemListContainer/itemListContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const itemListContainer = () => {
+  return (
+    <div>itemListContainer</div>
+  )
+}
+
+export default itemListContainer
