3d4108a9c8443111e78d376520c48f94d661dbc1
spawnew <96084099+spawnew@users.noreply.github.com>
2023-11-23 18:17:22 -0300

    areglando los productos

--- src/components/asyncMock.js
+++ src/components/asyncMock.js
@@ -1,11 +1,11 @@
 const products = [
-    { id: "1", name: "darknecrofear", img: "url-img", price: "100", category: "singles", description: "fiend", stock: "10" },
-    { id: "2", name: "Galaxy eyes", img: "url-img", price: 100, category: "singles", description: "dragon", stock: 10 },
-    { id: "3", name: "Ojos rojos ", img: "url-img", price: 100, category: "singles", description: "dragon oscurito", stock: 10 },
-    { id: "4", name: "blue eyes", img: "url-img", price: 100, category: "singles", description: "dragon blanco", stock: 10 },
-    { id: "5", name: "megatin", img: "url-img", price: 100, category: "sellados", description: "caja dorada", stock: 10 },
-    { id: "6", name: "rarity colection", img: "url-img", price: 100, category: "sellados", description: "caja normal", stock: 10 },
-    { id: "7", name: "batle of legend", img: "url-img", price: 100, category: "sellados", description: "caja plateada", stock: 10 },
+    { id: "1", name: "darknecrofear", img: "../../public/img/dark necrofear.jpg", price: "100", category: "singles", description: "fiend", stock: "10" },
+    { id: "2", name: "Galaxy eyes", img: "../../public/img/galaxy.jpeg", price: 100, category: "singles", description: "dragon", stock: 10 },
+    { id: "3", name: "Ojos rojos ", img: "../../public/img/ojos rojos.jpg", price: 100, category: "singles", description: "dragon oscurito", stock: 10 },
+    { id: "4", name: "blue eyes", img: "../../public/img/azules.jpeg", price: 100, category: "singles", description: "dragon blanco", stock: 10 },
+    { id: "5", name: "megatin", img: "../../public/img/descarga (1).jpeg", price: 100, category: "sellados", description: "caja dorada", stock: 10 },
+    { id: "6", name: "rarity colection", img: "../../public/img/descarga (2).jpeg", price: 100, category: "sellados", description: "caja normal", stock: 10 },
+    { id: "7", name: "batle of legend", img: "../../public/img/descarga (3).jpeg", price: 100, category: "sellados", description: "caja plateada", stock: 10 },
   ];
     
   export const getProducts = () => {
