48715cf0a73697cd9121d550efff486128923edd
spawnew <96084099+spawnew@users.noreply.github.com>
2023-11-23 12:34:00 -0300

    arreglado las import

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,20 @@
-import { ItemDetailContainer} from "./components";
-import {ItemListContainer} from "./components";
-import  {NavBar } from "./components";
-import { BrowserRouter} from "./components";
-import {Route} from "react-router-dom";
- import { Routes } from "react-router-dom";
+import { Cart, ItemDetailContainer, ItemListContainer, NavBar } from "./components";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-function App () {
-  
+export const App = () => {
   return (
-  
-      <BrowserRouter>
+    <BrowserRouter>
       <NavBar />
       <Routes>
         <Route path="/" element={<ItemListContainer />} />
         <Route path="/category/:category" element={<ItemListContainer />} />
         <Route path="/item/:id" element={<ItemDetailContainer />} />
-        
-        
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </BrowserRouter>
-      
-      
-  
-  )
-}
-export default App;
+  );
+};
+
 
 
 
--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,25 +1,18 @@
 import { Link } from "react-router-dom";
 
-export const Item = ({id, name, img, price }) => {
-    return (
-      <>
-      <div className="border m-2 col-md-3">
-        <div className="card col-12" >
-          <div className="card-body">
-            <h5 className="card-title">{name}</h5>
-            <div >
-              <img src={img} alt="" width={"450px"} height={"450px"}/>
-            </div>
-            <h4>${price}</h4>
-            <div className="d-flex flex-row-reverse">
-            <Link to={`./item/${id}`}>
-              <button className="btn btn-primary ">Detalles</button>
-            </Link>
-            </div>
-          </div>
+export const Item = ({id, name, img, description }) => {
+  return (
+    <div className="border m-2">
+      <div className="card ">
+        <div className="card-body ">
+          <h5 className="card-title">{name}</h5>
+          <img src={img} alt="" />
+          <p className="card-text"> {description} </p>
+          <Link to={`/item/${id}`}>
+            <button className="btn btn-outline-success">Detalles</button>
+          </Link>
         </div>
       </div>
-      
-      </>
-    );
-  };
+    </div>
+  );
+};
